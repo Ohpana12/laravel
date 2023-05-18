86145d82f91421ebe0aba55f8330c264e6952808
Ohpana12 <dawidpodoba@onet.pl>
2023-05-19 00:45:58 +0200

    laravel

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\peopleController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/127.0.0.1/Podoba/50536/people', [peopleController::class, 'index']);
+Route::get('/127.0.0.1/Podoba/50536/people/{id}', [peopleController::class, 'show']);
+Route::post('/127.0.0.1/Podoba/50536/people', [peopleController::class, 'create']);
+Route::put('/127.0.0.1/Podoba/50536/people/{id}', [peopleController::class, 'update']);
+Route::delete('/127.0.0.1/Podoba/50536/people/{id}', [peopleController::class, 'delete']);
